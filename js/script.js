cb42a19a9746092bbd9b42bc8d59fb97ef0322a1
jowonyoung1 <jwy7254@naver.com>
2024-09-18 21:03:53 +0900

    저작권 안내 팝업창 추가, 리드미 수정

--- js/script.js
+++ js/script.js
@@ -244,3 +244,12 @@ window.onload = function () {
     effectText();
   });
 };
+
+// 저작권 안내 팝업창
+window.addEventListener("load", function () {
+  const closeBt = document.querySelector(".popup-close");
+  const popup = document.querySelector(".popup");
+  closeBt.addEventListener("click", function () {
+    popup.style.display = "none";
+  });
+});
